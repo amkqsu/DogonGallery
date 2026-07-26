5a86fb026e275c61e2437e2d31626b4de1a9e8c7
Dogan <dogantema@gmail.com>
2026-07-26 07:13:14 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,3 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
-dependencies {
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-    implementation("com.google.mlkit:image-labeling:17.0.9")
-}
